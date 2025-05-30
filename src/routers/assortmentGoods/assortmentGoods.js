fa91c3feaddcdb9ce10d91c57fd1d85edee6f3b7
Andrew <andrejpopkov565@gmail.com>
2025-05-30 16:26:29 +0300

    unnecessary removed, added route for good inf

--- src/routers/assortmentGoods/assortmentGoods.js
+++ src/routers/assortmentGoods/assortmentGoods.js
@@ -5,11 +5,35 @@ const router = Router();
 
 router.get('/', async (req, res) => {
   try {
-    const data = await pool.query('SELECT * from assortment_goods');
+    const data = await pool.query(
+      'SELECT id, name, price, good_picture from assortment_goods',
+    );
+
     res.json({
       status: 200,
       message: 'Successfully completed operation',
       data: data.rows,
+    });
+  } catch (error) {
+    res.json({
+      status: 500,
+      message: error.message,
+    });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const data = await pool.query(
+      'select * from assortment_goods where id = $1',
+      [id],
+    );
+
+    res.json({
+      status: 200,
+      message: 'Successfully completed operation',
+      data: data.rows[0],
     });
   } catch (error) {
     res.json({
--- src/routers/users/user.js
+++ src/routers/users/user.js
@@ -20,49 +20,4 @@ router.get('/', async (req, res) => {
   }
 });
 
-// current user information
-
-router.get('/:userId', async (req, res) => {
-  const { userId } = req.params;
-  try {
-    const data = await pool.query('SELECT * FROM public.users WHERE id = $1', [
-      userId,
-    ]);
-    res.json({
-      status: 200,
-      message: 'Successfully completed operation',
-      data: data.rows,
-    });
-  } catch (error) {
-    res.status(500).json({ status: 500, message: error.message });
-  }
-});
-
-// delete user
-
-router.delete('/:userId', async (req, res) => {
-  try {
-    const { userId } = req.params;
-
-    if (!userId) {
-      return res
-        .status(400)
-        .json({ status: 400, message: 'userId is required' });
-    }
-
-    const data = await pool.query(
-      'DELETE FROM public.users WHERE id = $1 RETURNING *',
-      [userId],
-    );
-
-    res.json({
-      status: 200,
-      message: 'Succsessfulle deleted a user',
-      data: data.rows[0],
-    });
-  } catch (error) {
-    res.status(500).json({ status: 500, message: error.message });
-  }
-});
-
 export default router;
