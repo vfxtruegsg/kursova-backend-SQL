d5be8c9c497942a2dad8f1aa5f09a5610819f748
Andrew <andrejpopkov565@gmail.com>
2025-06-02 22:32:24 +0300

    resolved some prodlems with getCartContents route

--- src/routers/orders/orders.js
+++ src/routers/orders/orders.js
@@ -27,4 +27,25 @@ router.post('/addToCart', async (req, res) => {
   }
 });
 
+router.get('/getCartContents', async (req, res) => {
+  const { userId } = req.query;
+  try {
+    const data = await pool.query(
+      'SELECT * FROM cart_items WHERE user_id = $1',
+      [userId],
+    );
+
+    res.json({
+      status: 200,
+      message: 'Successfully found products for order',
+      data: data.rows,
+    });
+  } catch (error) {
+    res.json({
+      status: 500,
+      message: error.message,
+    });
+  }
+});
+
 export default router;
